eab1a6d856ae59162147b9d12bb8b541887b1ef8
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-08-12 11:56:44 +0300

    Add files via upload
    Added variables of Min and Max, assigned their values

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard, yo");
         Debug.Log("Pick a number, but don't tell me what it is");
-        Debug.Log("Highest number you may pick is 1000");
-        Debug.Log("Lowest number you may pick is 1");
+        Debug.Log("Highest number you may pick is: " + max);
+        Debug.Log("Lowest number you may pick is: " + min);
     }
 
     // Update is called once per frame
