d6f4f7db45a211d381738990bb0e22be94e8ae4a
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-08-12 12:47:01 +0300

    Add files via upload
    Added Response To Player Inputs
    Added If, Else If statements

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -10,15 +10,29 @@ public class NumberWizard : MonoBehaviour
         int max = 1000;
         int min = 1;
 
+
         Debug.Log("Welcome to Number Wizard, yo");
         Debug.Log("Pick a number, but don't tell me what it is");
         Debug.Log("Highest number you may pick is: " + max);
         Debug.Log("Lowest number you may pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You hit Enter.");
+        }
     }
 }
