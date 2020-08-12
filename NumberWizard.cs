0b7dc79e25300f708aad54c70b950f802880d464
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-08-12 11:55:37 +0300

    Add files via upload
    Print To Console With Debug.Log()

---
+++ NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard, yo");
+        Debug.Log("Pick a number, but don't tell me what it is");
+        Debug.Log("Highest number you may pick is 1000");
+        Debug.Log("Lowest number you may pick is 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
